273b228a50feefef2d25ea7985b136016676a45b
CaicMateus <caicmateusfreitas@gmail.com>
2024-03-29 00:30:44 -0300

    adiciona continuação da estilização da pagina de login

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -4,13 +4,20 @@ import './login.css'
 function LoginForm() {
   return (
     <div className="login-container">
-      <h2>Login</h2>
-      <form action="#">
-        <input type="text" placeholder="Username" required />
-        <input type="password" placeholder="Password" required />
-        <input type="submit" value="Login" />
-      </form>
+    <button className="google-login-button" /*onClick={handleGoogleLogin}*/>
+        <img src="../../public/img/google-37.png" alt="Google Logo" className="google-logo" />
+        <span>Fazer login com o Google</span>
+    </button>
+    <form action="#">
+        <input type="text" className="input-field" placeholder="Username" required />
+        <input type="password" className="input-field" placeholder="Password" required />
+        <input type="submit" className="input-field submit-button" value="Avançar" />
+    </form>
+    <div className="signup-text">
+    Não tem uma conta? <a href="#">Inscreva-se</a>
     </div>
+    </div>
+
   );
 }
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import LoginForm from '../components/Login/login'
+import '../components/Shared/app.css';
 
 export function Login() {
   return (
     <>
-      <div>
-        <LoginForm/>
+      <div className="app-container">
+      <div className="header">
+        <img src="../../public/img/Ft_cu.png" alt="Imagem 1" />
+        <img src="../../public/img/Ft_Commu Unity.png" alt="Imagem 2" />
+      </div>
+      <LoginForm />
       </div>
     </>
   )
