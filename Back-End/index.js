659f3e930c5ed043de6e4057395e4cd203fab25d
CaicMateus <caicmateusfreitas@gmail.com>
2024-05-31 18:09:57 -0300

    Adiciona alterações no backend para chamada no firebase

---
+++ Back-End/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import admin from 'firebase-admin'
+
+const app = express();
+
+admin.initializeApp({
+    credential: admin.credential.cert("database.json")
+})
+app.get("/users", (request,response) =>{
+    console.log('GET');
+    admin.firestore()
+    .collection('Users')
+    .get()
+    .then(snapshot => {
+        const users = snapshot.docs.map(doc => ({
+            ...doc.data(),
+            uid: doc.id
+        }))
+        response.json(users)
+    })
+    
+})
+
+app.listen(3000,() => console.log('foi.'))
